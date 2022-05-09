9a2885821b1264df508519d3ef17de699f9d013f
Cedric Heijlman <90176813+cedricheijlman@users.noreply.github.com>
2022-05-09 12:54:38 +0200

    change floor up down functionality

--- src/App.tsx
+++ src/App.tsx
@@ -3,8 +3,7 @@ import logo from "./logo.svg";
 import "./App.css";
 
 const App: React.FC = () => {
-  const [currentFloor, setCurrentFloor] = useState(0);
-
+  // All floors
   const floors: Object[] = [
     { floorNum: 0 },
     { floorNum: 1 },
@@ -14,21 +13,62 @@ const App: React.FC = () => {
     { floorNum: 5 },
   ];
 
+  // Current floor
+  const [currentFloor, setCurrentFloor] = useState<number>(0);
+
+  // Change floor up and down
+  const changeFloor = (floorNum: number, action: string): void => {
+    const oldFloorNum = floorNum;
+    if (action == "up" && floorNum < 5) {
+      setTimeout(() => {
+        setCurrentFloor(oldFloorNum + 1);
+      }, 1000);
+    }
+
+    if (action == "down" && floorNum > 0) {
+      setTimeout(() => {
+        setCurrentFloor(oldFloorNum - 1);
+      }, 1000);
+    }
+  };
+
   return (
     <div className="container">
       <h1>Elevator</h1>
       <div className="elevatorContainer">
         {floors.reverse().map((floor: any) => {
+          const floorNumber = floor.floorNum;
           return (
-            <div
-              className={
-                currentFloor == floor.floorNum
-                  ? "singleFloor current"
-                  : "singleFloor"
-              }
-            >
-              <p>{floor.floorNum}</p>
-              <div></div>
+            <div className="singleFloorContainer">
+              {currentFloor == floorNumber ? (
+                <div className="upDownButtons">
+                  <p
+                    onClick={() => {
+                      changeFloor(floorNumber, "up");
+                    }}
+                  >
+                    Up
+                  </p>
+                  <p
+                    onClick={() => {
+                      changeFloor(floorNumber, "down");
+                    }}
+                  >
+                    Down
+                  </p>
+                </div>
+              ) : (
+                <div></div>
+              )}
+              <div
+                className={
+                  currentFloor == floor.floorNum
+                    ? "singleFloor current"
+                    : "singleFloor"
+                }
+              >
+                <p>{floor.floorNum}</p>
+              </div>
             </div>
           );
         })}
